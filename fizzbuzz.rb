a5f52807b1b4df14b965d3bafd85eacdfa1eabd4
Sean Conrad <seanconrad123@gmail.com>
2018-05-30 20:09:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(n)
-  if n % 3 == 0
+  
+  if n % 5 == 0 && n % 3 == 0
+    return "FizzBuzz"
+  elsif n % 3 == 0
     return "Fizz"
   elsif n % 5 == 0
     return "Buzz"
-  elsif n % 5 == 0 && n % 3 == 0
-    return "FizzBuzz"
+
   end
 end
