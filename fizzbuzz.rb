11f46a43702e9ac19a51a03a7c09159134c9a0be
Sean Conrad <seanconrad123@gmail.com>
2018-05-30 20:06:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 def fizzbuzz(n)
-  puts "hello"
+  if (n % 3 == 0)
+    puts "Fizz"
+  end
+  elsif (n % 5 == 0)
+    puts "Buzz"
+  end
+  elsif (n % 15 == 0)
+    puts "FizzBuzz"
+  end
 end
 
-fizzbuzz(2)
+fizzbuzz(3)
